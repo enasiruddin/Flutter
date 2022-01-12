f9f142c71006802ea370a5765cc4bae08f2e0a33
enasiruddin <nasiruddinpust@gmail.com>
2022-01-12 18:04:36 +0600

    'firebaseupdate'

--- Projects/travel_guide/lib/pages/crud.dart
+++ Projects/travel_guide/lib/pages/crud.dart
@@ -10,6 +10,64 @@ class Crud extends StatefulWidget {
 }
 
 class _CrudState extends State<Crud> {
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailTextController = TextEditingController();
+
+  final CollectionReference _user =
+  FirebaseFirestore.instance.collection('user');
+
+  Future<void> _create() async {
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(labelText: 'Name',labelStyle: TextStyle(color: Colors.blue)),
+                ),
+                TextField(
+                  controller: _emailTextController,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.blue)
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text("Create"),
+                  onPressed: () async {
+                    final String? name = _nameController.text;
+                    final String? email = _emailTextController.text;
+                    if (name != null && email != null) {
+                      await _user.add({"name": name, "email": email});
+
+                      _nameController.text = '';
+                      _emailTextController.text = '';
+
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _delete (String userid) async {
+    await _user.doc(userid).delete();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +97,9 @@ class _CrudState extends State<Crud> {
                                 icon: Icon(Icons.edit),
                               ),
                               IconButton(
-                                onPressed: (){},
+                                onPressed: (){
+                                  _delete(networkData.id);
+                                },
                                 icon: Icon(Icons.delete),
                               ),
                             ],
@@ -56,6 +116,12 @@ class _CrudState extends State<Crud> {
 
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _create();
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
